7d127a9f52f6ed5d0bcd8ef55e89677a0487201a
Vellum__ <lukasrichtergeschaeftlich@gmail.com>
2026-01-07 02:41:22 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
+      base: "/portfolio-website/",
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
